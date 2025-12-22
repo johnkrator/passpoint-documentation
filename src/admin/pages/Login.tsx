ea7e722b93869bdb75b31ab3902fef8b019c2f2e
0xc2god <chukwuchidieberejohn@gmail.com>
2025-12-22 02:57:53 +0100

    add logo to admin login page and update description

--- src/admin/pages/Login.tsx
+++ src/admin/pages/Login.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import FormInput from '@/admin/components/FormInput';
 import { LogIn } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
+import Logo from "@/assets/passpoint-logo.webp";
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -21,12 +22,9 @@ const Login = () => {
             <div className='w-full max-w-md'>
                 <div className='bg-card border border-border rounded-lg shadow-lg p-8'>
                     <div className='flex flex-col items-center mb-8'>
-                        <div className='w-16 h-16 bg-brand rounded-lg flex items-center justify-center mb-4'>
-                            <span className='text-white font-bold text-2xl'>PP</span>
-                        </div>
-                        <h1 className='text-2xl font-semibold text-foreground'>Admin Login</h1>
+                        <img src={Logo} className="h-8 sm:h-10 w-auto object-contain" alt="Passpoint Logo"/>
                         <p className='text-sm text-muted-foreground mt-1'>
-                            Sign in to manage your documentation
+                            Sign in as an admin to manage documentation
                         </p>
                     </div>
 
